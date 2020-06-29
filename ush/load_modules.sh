3ff2b034e65f6bb4c2a1f7829ba38a16c57354bb
Mallory Row <mallory.row@noaa.gov>
2020-06-29 13:41:37 -0500

    Adjust which statements

--- ush/load_modules.sh
+++ ush/load_modules.sh
@@ -151,12 +151,17 @@ else
     echo "ERROR: $machine is not supported"
     exit 1
 fi
-export NCAP2=`which ncap2`
-export NCDUMP=`which ncdump`
 if [ $machine != "ORION" ]; then
+    export NCAP2=`which ncap2`
+    export NCDUMP=`which ncdump`
     export HTAR=`which htar`
+    export CONVERT=`which convert`
 fi
-export CONVERT=`which convert`
+if [ $machine = "ORION" ]; then
+    export NCAP2=`which ncap2 | sed 's/ncap2 is //g'`
+    export NCDUMP=`which ncdump | sed 's/ncdump is //g'`
+    export CONVERT=`which convert | sed 's/convert is //g'`
+fi
 echo "Using HOMEMET=${HOMEMET}"
 echo "Using HOMEMETplus=${HOMEMETplus}"
 

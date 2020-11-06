5f8bafb86859d6f29e73397aad805d9b29dbb70b
Mallory Row <mallory.row@noaa.gov>
2020-11-06 14:18:05 +0000

    Add checks for rest of RUNs in check_config.py

--- ush/check_config.py
+++ ush/check_config.py
@@ -157,15 +157,166 @@ elif RUN == 'grid2grid_step2':
         valid_config_var_values_dict[RUN_abbrev_type+'_event_eq'] = [
             'True', 'False'
         ]
-#elif RUN == 'grid2obs_step1':
-#elif RUN == 'grid2obs_step2':
-#elif RUN == 'precip_step1':
-#elif RUN == 'precip_step2':
-#elif RUN == 'satellite_step1':
-#elif RUN == 'satellite_step2':
-#elif RUN == 'tropcyc':
-#elif RUN == 'maps2d':
-#elif RUN == 'mapsda':
+elif RUN == 'grid2obs_step1':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        if RUN_type == 'polar_sfc':
+            valid_config_var_values_dict[RUN_abbrev_type+'_obtype_list'] = [
+                'IABP'
+            ]
+        else:
+            valid_config_var_values_dict[RUN_abbrev_type+'_obtype_list'] = [
+                 'ADPUPA', 'AIRCAR', 'AIRCFT', 'ADPSFC', 'ERS1DA',
+                 'GOESND', 'GPSIPW', 'MSONET', 'PROFLR', 'QKSWND',
+                 'RASSDA', 'SATEMP', 'SATWND', 'SFCBOG', 'SFCSHP',
+                 'SPSSMI', 'SYNDAT', 'VADWND', 'SURFACE', 'ANYAIR',
+                 'ANYSFC', 'ONLYSF'
+             ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+    valid_config_var_values_dict[RUN_abbrev+'_prepbufr_data_run_hpss'] = [
+        'YES', 'NO'
+    ]
+elif RUN == 'grid2obs_step2':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        if RUN_type == 'polar_sfc':
+            valid_config_var_values_dict[RUN_abbrev_type+'_obtype_list'] = [
+                'IABP'
+            ]
+        else:
+            valid_config_var_values_dict[RUN_abbrev_type+'_obtype_list'] = [
+                 'ADPUPA', 'AIRCAR', 'AIRCFT', 'ADPSFC', 'ERS1DA',
+                 'GOESND', 'GPSIPW', 'MSONET', 'PROFLR', 'QKSWND',
+                 'RASSDA', 'SATEMP', 'SATWND', 'SFCBOG', 'SFCSHP',
+                 'SPSSMI', 'SYNDAT', 'VADWND', 'SURFACE', 'ANYAIR',
+                 'ANYSFC', 'ONLYSF'
+             ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by_list'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_event_eq'] = [
+            'True', 'False'
+        ]
+elif RUN == 'precip_step1':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_model_varname_list'] = [
+            'APCP', 'PRATE'
+        ]
+        RUN_abbrev_type_accum_length = (
+            RUN_type.split('accum')[1].replace('hr','')
+        )
+        for model_bucket in \
+                os.environ[RUN_abbrev_type+'_model_bucket_list'].split(' '):
+            if int(model_bucket) > int(RUN_abbrev_type_accum_length):
+                print("ERROR: value of "+model_bucket+" in "
+                      +RUN_abbrev_type+"_model_bucket_list must be <= to "
+                      +RUN_abbrev_type+" accumulation length which is "
+                      +RUN_abbrev_type_accum_length)
+                exit(1)
+    valid_config_var_values_dict[RUN_abbrev+'_obs_data_run_hpss'] = [
+        'YES', 'NO'
+    ]
+elif RUN == 'precip_step2':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by_list'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_event_eq'] = [
+            'True', 'False'
+        ]
+elif RUN == 'satellite_step1':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+        if float(os.environ[RUN_abbrev_type+'_sea_ice_thresh']) > 1:
+            print("ERROR: value of "+RUN_abbrev_type+"_sea_ice_thresh "
+                  +"must be <= 1")
+            exit(1)
+elif RUN == 'satellite_step2':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_gather_by_list'] = [
+            'VALID', 'INIT', 'VSDB'
+        ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_event_eq'] = [
+            'True', 'False'
+        ]
+        if float(os.environ[RUN_abbrev_type+'_sea_ice_thresh']) > 1:
+            print("ERROR: value of "+RUN_abbrev_type+"_sea_ice_thresh "
+                  +"must be <= 1")
+            exit(1)
+elif RUN == 'tropcyc':
+    for atcf_name in os.environ[RUN+'_model_atcf_name_list'].split(' '):
+        if len(atcf_name) != 4:
+            print("ERROR: length of "+atcf_name+" in "
+                  +RUN+"_model_atcf_name_list != to 4")
+            exit(1)
+elif RUN == 'maps2d':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_make_met_data_by'] = [
+            'VALID', 'INIT'
+        ]
+        valid_config_var_values_dict[RUN_abbrev_type+'_anl_name'] = [
+            'self_anl', 'self_f00', 'gfs_anl', 'gfs_f00'
+        ]
+        if RUN_type == 'model2model':
+            valid_config_var_values_dict[RUN_abbrev_type
+                                         +'_forecast_anl_diff'] = [
+                'YES', 'NO'
+            ]
+        elif RUN_type == 'model2obs':
+            valid_config_var_values_dict[RUN_abbrev_type+'_use_ceres'] = [
+                'YES', 'NO'
+            ]
+            valid_config_var_values_dict[RUN_abbrev_type
+                                         +'_use_monthly_mean'] = [
+                'YES', 'NO'
+            ]
+        for forecast_to_plot \
+                in os.environ[RUN_abbrev_type
+                              +'_forecast_to_plot_list'].split(' '):
+            if forecast_to_plot[0] == 'a':
+                if forecast_to_plot != 'anl':
+                    print("ERROR: value of "+forecast_to_plot+" in "
+                          +RUN_abbrev_type+"_forecast_to_plot_list must be "
+                          +"anl to use analysis")
+                    exit(1)
+            elif forecast_to_plot[0] in ['d', 'f']:
+                if not forecast_to_plot[1:].isnumeric():
+                    print("ERROR: value of "+forecast_to_plot[1:]+" in "
+                          +forecast_to_plot+" in "+RUN_abbrev_type
+                          +"_forecast_to_plot_list must be numeric")
+                    exit(1)
+            else:
+                print("ERROR: value of "+forecast_to_plot+" in "
+                      +RUN_abbrev_type+"_forecast_to_plot_list must be either "
+                      +"anl or dX or fX, where X is a number")
+                exit(1)
+elif RUN == 'mapsda':
+    for RUN_type in RUN_type_list:
+        RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
+        valid_config_var_values_dict[RUN_abbrev_type+'_make_met_data_by'] = [
+            'VALID', 'INIT'
+        ]
+        if RUN_type == 'ens':
+            valid_config_var_values_dict[RUN_abbrev_type
+                                         +'_netcdf_suffix_list'] = [
+                'nc', 'nc4'
+            ]
+            valid_config_var_values_dict[RUN_abbrev_type
+                                         +'_model_data_run_hpss'] = [
+                'YES', 'NO'
+            ]
 for config_var in list(valid_config_var_values_dict.keys()):
     if 'list' in config_var:
         for list_item in os.environ[config_var].split(' '):

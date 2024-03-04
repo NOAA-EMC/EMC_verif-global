30f22463e82e22539ccead1de6d4aac3a1ceaf5f
Mallory Row <mallory.row@noaa.gov>
2024-03-04 16:51:53 +0000

    Update Hera module file for Rocky 8

--- modulefiles/emc_verif_global_hera.lua
+++ modulefiles/emc_verif_global_hera.lua
@@ -10,10 +10,10 @@ prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/mo
 hpc_ver=os.getenv("hpc_ver") or "1.1.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
 load(pathJoin("hpc-intel", hpc_intel_ver))
 
-hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
+hpc_impi_ver=os.getenv("hpc_impi_ver") or "2022.1.2"
 load(pathJoin("hpc-impi", impi_ver))
 
 netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
@@ -25,7 +25,7 @@ load(pathJoin("nco", nco_ver))
 prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 load(pathJoin("prod_util", prod_util_ver))
 
-grib_util_ver=os.getenv("grib_util_ver") or "1.2.2"
+grib_util_ver=os.getenv("grib_util_ver") or "1.2.3"
 load(pathJoin("grib_util", grib_util_ver))
 
 grads_ver=os.getenv("grads_ver") or "2.2.1"

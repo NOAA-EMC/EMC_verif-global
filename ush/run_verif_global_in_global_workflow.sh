0a3254a08e163ed9d687d5395b84eb9103ab758b
Kate.Friedman <Kate.Friedman@noaa.gov>
2025-06-25 16:47:42 +0000

    Update obsproc to v1.2
    - Add obsproc_ver to replace hardcoded version
    
    Refs #130

--- ush/run_verif_global_in_global_workflow.sh
+++ ush/run_verif_global_in_global_workflow.sh
@@ -299,9 +299,9 @@ elif [ $machine = "GAEAC6" ]; then
 fi
 
 ## Set operational directories
-export prepbufr_prod_upper_air_dir="/lfs/h1/ops/prod/com/obsproc/v1.1"
-export prepbufr_prod_conus_sfc_dir="/lfs/h1/ops/prod/com/obsproc/v1.1"
-export ccpa_24hr_prod_dir="/lfs/h1/ops/prod/com/verf_precip/v4.5"
+export prepbufr_prod_upper_air_dir="/lfs/h1/ops/prod/com/obsproc/${obsproc_ver}"
+export prepbufr_prod_conus_sfc_dir="/lfs/h1/ops/prod/com/obsproc/${obsproc_ver}"
+export ccpa_24hr_prod_dir="/lfs/h1/ops/prod/com/verf_precip/${verf_precip_ver}"
 
 ## Some online sites
 export iabp_ftp="http://iabp.apl.washington.edu/Data_Products/Daily_Full_Res_Data"

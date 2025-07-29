8c4f52355f46a1831156013e8e60c925f7dca521
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2025-07-29 15:25:36 -0400

    Set precip1_ccpa_accum24hr_fhr_max=240

--- parm/config/emulate_gw_standalone.sh
+++ parm/config/emulate_gw_standalone.sh
@@ -204,7 +204,7 @@ export precip1_ccpa_accum24hr_model_file_format="pgbf{lead?fmt=%2H}.${RUN}.{init
 # If no forecast files are missing, precip1_ccpa_accum24hr_fhr_min="24" and
 # precip1_ccpa_accum24hr_fhr_max="240" (where 240 is the longest forecast)
 export precip1_ccpa_accum24hr_fhr_min="48"
-export precip1_ccpa_accum24hr_fhr_max="132"
+export precip1_ccpa_accum24hr_fhr_max="240"
 export precip1_ccpa_accum24hr_grid="G211"
 export precip1_ccpa_accum24hr_gather_by="VSDB"
 export precip1_obs_data_run_hpss="NO"

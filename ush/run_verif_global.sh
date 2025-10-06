ee25371b30d34b834b52e90ceb09be9ce63cf7ef
Shannon Shields <Shannon.Shields@gaea62.ncrc.gov>
2025-10-06 13:19:23 -0400

    Remove step1 lines in run_verif_global.sh

--- ush/run_verif_global.sh
+++ ush/run_verif_global.sh
@@ -14,8 +14,8 @@ echo "=============== SOURCING CONFIGS ==============="
 passed_config=$1
 passed_config_strlength=$(echo -n $passed_config | wc -m)
 if [ $passed_config_strlength = 0 ]; then
-    echo "No config passed, using default: $HOMEverif_global/parm/config/config.vrfy"
-    config=$HOMEverif_global/parm/config/config.vrfy
+    echo "No config passed, using default: $HOMEverif_global/parm/config/driver_standalone_step2_plots.sh"
+    config=$HOMEverif_global/parm/config/driver_standalone_step2_plots.sh
 else
     config=$(readlink -f $passed_config)
     if [ ! -e $config ]; then
@@ -37,67 +37,35 @@ status=$?
 [[ $status -ne 0 ]] && exit $status
 [[ $status -eq 0 ]] && echo "Succesfully ran set_up_verif_global.sh"
 
-echo "=============== RUNNING METPLUS ==============="
-if [ $RUN_GRID2GRID_STEP1 = YES ] ; then
-    echo
-    echo "===== RUNNING GRID-TO-GRID STEP 1 VERIFICATION  ====="
-    echo "===== creating partial sum data for grid-to-grid verifcation using METplus ====="
-    export RUN="grid2grid_step1"
-    python $HOMEverif_global/ush/run_batch.py
-fi
 
 if [ $RUN_GRID2GRID_STEP2 = YES ] ; then
     echo
     echo "===== RUNNING GRID-TO-GRID STEP 2 VERIFICATION  ====="
-    echo "===== calculating statistics and creating plots for grid-to-grid verifcation using METplus ====="
+    echo "===== calculating statistics and creating plots for grid-to-grid verification ====="
     export RUN="grid2grid_step2"
-    python $HOMEverif_global/ush/run_batch.py
-fi
-
-if [ $RUN_GRID2OBS_STEP1 = YES ] ; then
-    echo
-    echo "===== RUNNING GRID-TO-OBSERVATIONS STEP 1 VERIFICATION  ====="
-    echo "===== creating partial sum data for grid-to-observations verifcation using METplus ====="
-    export RUN="grid2obs_step1"
     python $HOMEverif_global/ush/run_batch.py
 fi
 
 if [ $RUN_GRID2OBS_STEP2 = YES ] ; then
     echo
     echo "===== RUNNING GRID-TO-OBSERVATIONS STEP 2 VERIFICATION  ====="
-    echo "===== calculating statistics and creating plots for grid-to-observations verifcation using METplus ====="
+    echo "===== calculating statistics and creating plots for grid-to-observations verification ====="
     export RUN="grid2obs_step2"
-    python $HOMEverif_global/ush/run_batch.py
-fi
-
-if [ $RUN_PRECIP_STEP1 = YES ] ; then
-    echo
-    echo "===== RUNNING PRECIPITATION STEP 1 VERIFICATION  ====="
-    echo "===== creating partial sum data for precipitation verifcation using METplus ====="
-    export RUN="precip_step1"
     python $HOMEverif_global/ush/run_batch.py
 fi
 
 if [ $RUN_PRECIP_STEP2 = YES ] ; then
     echo
     echo "===== RUNNING PRECIPITATION STEP 2 VERIFICATION  ====="
-    echo "===== calculating statistics and creating plots for precipitation verifcation using METplus ====="
+    echo "===== calculating statistics and creating plots for precipitation verification ====="
     export RUN="precip_step2"
-    python $HOMEverif_global/ush/run_batch.py
-fi
-
-if [ $RUN_SATELLITE_STEP1 = YES ] ; then
-    echo
-    echo "===== RUNNING SATELLITE STEP 1 VERIFICATION  ====="
-    echo "===== creating partial sum data for satellite verifcation using METplus ====="
-    export RUN="satellite_step1"
     python $HOMEverif_global/ush/run_batch.py
 fi
 
 if [ $RUN_SATELLITE_STEP2 = YES ] ; then
     echo
     echo "===== RUNNING SATELLITE STEP 2 VERIFICATION  ====="
-    echo "===== calculating statistics and creating plots for satellite verifcation using METplus ====="
+    echo "===== calculating statistics and creating plots for satellite verification ====="
     export RUN="satellite_step2"
     python $HOMEverif_global/ush/run_batch.py
 fi
@@ -128,7 +96,7 @@ fi
 
 if [ $RUN_MAPSDA = YES ] ; then
     echo
-    echo "===== RUNNING GDAS ANALYSIS AND ENSEMBLE COMAPRISON VERIFICATION  ====="
+    echo "===== RUNNING GDAS ANALYSIS AND ENSEMBLE COMPARISON VERIFICATION  ====="
     export RUN="mapsda"
     python $HOMEverif_global/ush/run_batch.py
 fi

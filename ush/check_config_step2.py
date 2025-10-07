824744a3d96284c4ed8099f8d6dc7b4db18b8f20
Shannon Shields <Shannon.Shields@gaea62.ncrc.gov>
2025-10-07 10:25:55 -0400

    Adjust truth file names and remove from config check

--- ush/check_config_step2.py
+++ ush/check_config_step2.py
@@ -307,17 +307,6 @@ if RUN == 'grid2grid_step2':
     for RUN_type in RUN_type_list:
         RUN_abbrev_type = RUN_abbrev+'_'+RUN_type
         valid_config_var_values_dict[RUN_abbrev_type
-                                     +'_truth_name_list'] = ['self_anl',
-                                                             'self_f00',
-                                                             'gfs_anl',
-                                                             'gfs_f00',
-                                                             'gdas_anl',
-                                                             'gdas_f00',
-                                                             'ecm_f00',
-                                                             'common_anl',
-                                                             'common_f00',
-                                                             'model_mean']
-        valid_config_var_values_dict[RUN_abbrev_type
                                      +'_gather_by_list'] = ['VALID', 'INIT',
                                                             'VSDB']
         valid_config_var_values_dict[RUN_abbrev_type

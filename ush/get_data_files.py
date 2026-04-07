19a1c597bbbcfcbd3ce75d14abdb6cc58ec12f1e
Mallory Row <mallory.row@noaa.gov>
2026-04-07 13:00:11 +0000

    Update way of looking for model files

--- ush/get_data_files.py
+++ ush/get_data_files.py
@@ -522,11 +522,9 @@ def get_model_file(valid_time_dt, init_time_dt, lead_str,
     if not os.path.exists(link_model_file):
         model_filename = format_filler(file_format, valid_time_dt,
                                        init_time_dt, lead_str)
-        #Uncomment the model_file line below if using default global archive
-        #model_file = os.path.join(data_dir, name, model_filename)
-        #Uncomment the model_file line below if ARCDIR contains
-        #the model experiment name
-        model_file = os.path.join(data_dir, model_filename)
+        model_file = os.path.join(data_dir, name, model_filename)
+        if not os.path.exists(model_file):
+            model_file = os.path.join(data_dir, model_filename)
         if os.path.exists(model_file):
             if check_file_type(model_file) in ['grib2', 'netcdf']:
                 os.system('ln -sf '+model_file+' '+link_model_file)

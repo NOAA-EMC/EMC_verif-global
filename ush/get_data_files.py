7ef24bacd8a2ee9e0f74f077728f46d18bf264e9
Mallory Row <mallory.row@noaa.gov>
2026-08-06 13:15:04 +0000

    Fix typos, part 2

--- ush/get_data_files.py
+++ ush/get_data_files.py
@@ -186,8 +186,8 @@ def wget_data(wget_job_filename, wget_job_name, wget_job_output):
             ['sbatch', '--nodes=1', '--ntasks-per-node=1',
              '--time='+walltime.strftime('%H:%M:%S'), '--cluster='+CLUSTERS_DTN,
              '--partition='+PARTITION_DTN, '--constraint=f6', '--qos=dtn',
-             +'--account='+ACCOUNT, '--output='+wget_job_output,
-             +'--job-name='+wget_job_name, wget_job_filename]
+             '--account='+ACCOUNT, '--output='+wget_job_output,
+             '--job-name='+wget_job_name, wget_job_filename]
         )
         job_check_cmd = ('squeue -u '+os.environ['USER']+' -n '
                          +wget_job_name+' -t R,PD -h | wc -l')
@@ -443,9 +443,9 @@ def get_hpss_data(hpss_job_filename, save_data_dir, save_data_file,
     if machine == 'WCOSS2':
         vfg_util.run_shell_command(
             ['qsub', '-l', 'walltime='+walltime.strftime('%H:%M:%S'),
-             +'-q', QUEUESERV, '-A', ACCOUNT, '-o', hpss_job_output,
-             +'-e', hpss_job_output, '-N', hpss_job_name,
-             +'-l', 'select=1:ncpus=1', hpss_job_filename]
+             '-q', QUEUESERV, '-A', ACCOUNT, '-o', hpss_job_output,
+             '-e', hpss_job_output, '-N', hpss_job_name,
+             '-l', 'select=1:ncpus=1', hpss_job_filename]
         )
         job_check_cmd = ('qselect -s QR -u '+os.environ['USER']+' '
                          +'-N '+hpss_job_name+' | wc -l')
